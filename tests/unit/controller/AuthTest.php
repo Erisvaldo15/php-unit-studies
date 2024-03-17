c7d31b06cac1ff255ee517fea4088ce06c9629c0
Erisvaldo15 <erisvaldosilvadesousa38@gmail.com>
2024-03-16 21:56:00 -0300

    Seventh class

--- tests/unit/controller/AuthTest.php
+++ tests/unit/controller/AuthTest.php
@@ -8,9 +8,23 @@ use PHPUnit\Framework\TestCase;
 
 class AuthTest extends TestCase {
 
+    public function setUp(): void {
+        echo "setUp method it's working. \n";
+    }
+
+    public function tearDown(): void {
+        echo "tearDown method it's working. \n";
+    }
+
     public function testAttempt() {
+        echo "attemp test \n";
         $validated = (new Validation)->validate([true]);
         $this->assertTrue((new Auth)->attempt($validated));
+    }
+
+    public function testLogout() {
+        echo "logout test \n";
+        $this->assertTrue(true);
     }
 
 }
